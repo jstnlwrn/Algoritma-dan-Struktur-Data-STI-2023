ec5cf00986dd97b6994b7f980ef69cbc98593fca
jstnlwrn <justinlawrance18@gmail.com>
2023-11-09 03:13:03 +0700

    revisi revisi revisi pra prak 9
    soal dajjal yg sabar

--- TP + PRAKTIKUM/prak 9/OddEvenList.c
+++ TP + PRAKTIKUM/prak 9/OddEvenList.c
@@ -6,39 +6,39 @@ void OddEvenList(List L, List *Odd, List *Even){
     CreateEmpty(Even);
     //declare temp
     address P = First(L);
-    address prec;
     address temp;
     //transverse list
     while(P != Nil){
-        //check odd or even
-        if(Info(P) % 2 == 0){
-            temp = First(*Even);
-            //check if list empty or first node is higher than current node
-            if(temp == Nil || Info(temp) > Info(P)){
-                InsVFirst(Even, Info(P));
-            //else, find value higher than info(P)
-            }else{
-                while(Info(Next(temp)) <= Info(P) || Next(temp) != Nil){
+        //check odd
+        if(Info(P) % 2 == 1){
+            temp = First(*Odd);
+            //check if list is empty
+            if(temp == Nil || Info(P) < Info(temp)){
+                //insert to first
+                InsVFirst(Odd, Info(P));
+            }
+            else{
+                //find info(next(temp)) > info(P)
+                while(Next(temp) != Nil && Info(Next(temp)) < Info(P)){
                     temp = Next(temp);
                 }
-                InsertAfter(Even, Alokasi(Info(P)), prec);
+                //insert after temp
+                InsertAfter(Odd, Alokasi(Info(P)), temp);
             }
         }else{
-            temp = First(*Odd);
-            //check if list empty or first node is higher than current node
-            if(temp == Nil || Info(temp) > Info(P)){
-                InsVFirst(Odd, Info(P));
-            //else, find value higher than info(P)
-            }else{
-                while(true){
-                    //if next node is higher than info(P)
-                    if(Info(Next(temp)) > Info(P) || Next(temp) == Nil){
-                        //insert after temp
-                        InsertAfter(Odd, Alokasi(Info(P)), temp);
-                        break;
-                    }
+            temp = First(*Even);
+            //check if list is empty
+            if(temp == Nil || Info(P) < Info(temp)){
+                //insert to first
+                InsVFirst(Even, Info(P));
+            }
+            else{
+                //find info(next(temp)) > info(P)
+                while(Next(temp) != Nil && Info(Next(temp)) < Info(P)){
                     temp = Next(temp);
                 }
+                //insert after temp
+                InsertAfter(Even, Alokasi(Info(P)), temp);
             }
         }
         //move to next element
@@ -46,27 +46,27 @@ void OddEvenList(List L, List *Odd, List *Even){
     }
 }
 
-int main(){
-    //declare list
-    List L;
-    List Odd;
-    List Even;
-    //create empty list
-    CreateEmpty(&L);
-    //insert L with random number
-    InsVLast(&L, 0);
-    InsVLast(&L, 5);
-    InsVLast(&L, 1);
-    InsVLast(&L, 4);
-    InsVLast(&L, 5);
-    InsVLast(&L, 2);
-    InsVLast(&L, 4);
-    InsVLast(&L, 7);
-    //odd even list
-    OddEvenList(L, &Odd, &Even);
-    //print list
-    PrintInfo(L);
-    PrintInfo(Odd);
-    PrintInfo(Even);
-    return 0;
-}
+// int main(){
+//     //declare list
+//     List L;
+//     List Odd;
+//     List Even;
+//     //create empty list
+//     CreateEmpty(&L);
+//     //insert L with random number
+//     InsVLast(&L, 0);
+//     InsVLast(&L, 5);
+//     InsVLast(&L, 1);
+//     InsVLast(&L, 4);
+//     InsVLast(&L, 5);
+//     InsVLast(&L, 2);
+//     InsVLast(&L, 4);
+//     InsVLast(&L, 7);
+//     //odd even list
+//     OddEvenList(L, &Odd, &Even);
+//     //print list
+//     PrintInfo(L);
+//     PrintInfo(Odd);
+//     PrintInfo(Even);
+//     return 0;
+// }
