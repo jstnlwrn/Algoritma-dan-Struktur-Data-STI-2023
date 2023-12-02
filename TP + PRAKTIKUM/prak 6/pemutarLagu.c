378541628e64ff31056365604fadbd86578d1535
jstnlwrn <justinlawrance18@gmail.com>
2023-12-02 18:51:40 +0700

    prak 6 pemutarlagu

--- TP + PRAKTIKUM/prak 6/pemutarLagu.c
+++ TP + PRAKTIKUM/prak 6/pemutarLagu.c
@@ -7,19 +7,11 @@ void play(Queue *Q, boolean loop){
         printf("Tidak ada lagu\n");
     }
     else{
-        //check if loop is true
-        if(loop){
-            //play song
-            ElType song = dequeue(Q);
-            printf("Play lagu %d\n", song);
-            //enqueue song
-            enqueue(Q, song);
+        //play song
+        if(loop && !IsFull(*Q)){
+            enqueue(Q, HEAD(*Q));
         }
-        else{
-            //play song
-            ElType song = dequeue(Q);
-            printf("Play lagu %d\n", song);
-        }
+        printf("Play lagu %d\n", dequeue(Q));
     }
 }
 
@@ -34,73 +26,50 @@ void loopSwitch(boolean *loop){
 }
 
 void queueSong(Queue *Q, int N){
+    //input song
+        ElType song;
+        scanf("%d", &song);
     //check if queue is full
     if(IsFull(*Q)){
         printf("Queue penuh\n");
-    }
-    else{
-        //input song
-        ElType song;
-        scanf("%d", &song);
+    }else{
         //enqueue song
         if(song >= 0 && song <= N - 1) enqueue(Q, song);
     }
 }
 
-void queuePlaylist(Queue *Q, Queue playlist){
-    //check if queue is full
-    if(IsFull(*Q)){
-        printf("Queue penuh\n");
-    }
-    else{
-        //get playlist ID
-        int ID;
-        scanf("%d", &ID);
-        ID = 3 + 3*ID; //lagu index ke ID-3 sampai ID-1
-        //check if playlist ID is valid
-        if(ID <= Length(playlist) && ID-3 >= 0){
-            //get song
-            ElType song;
-            int i, j;
-            for(i = ID-3; i<= ID-1; i++){
-                j = (IDX_HEAD(playlist) + i) % (IDX_MAX + 1);
-                song = playlist.Tab[j];
-                //enqueue song
-                enqueue(Q, song);
-                if(IsFull(*Q)){
-                    printf("Queue penuh\n");
-                    break;
-                }
-            }
+void queuePlaylist(Queue *Q, int playlist[][3]){
+    int playlist_id;
+    scanf("%d", &playlist_id);
+    int i;
+    for (int i = 0; i < 3; i++) {
+        if (!IsFull(*Q)) {
+            enqueue(Q, playlist[playlist_id][i]);
+        } else {
+            printf("Queue penuh\n");
+            break;
         }
-    }
+    }    
 }
 
 int main(){
     //get N, M value
     int N, M;
     scanf("%d %d\n", &N, &M);
-    if (N < 1 || M < 0 || M > 100){
-        printf("Input tidak valid\n");
-        return 0;
-    } 
-    //create playlist
-    Queue playlist;
-    CreateQueue(&playlist);
-    //input song
-    ElType song;
+
+    //input playlist
     int i, j;
-    for(i = 0; i < M; i++){
-        for(j = 0; j < 3; j++){
-            scanf("%d", &song);
-            //check if song is valid
-            if(song < 0 || song > N - 1){
+    int playlist[M][3];
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &playlist[i][j]);
+            if (playlist[i][j] < 0 || playlist[i][j] > N - 1) {
                 printf("Playlist tidak valid\n");
                 return 0;
             }
-            enqueue(&playlist, song);
         }
     }
+
     //create queue
     Queue Q;
     CreateQueue(&Q);
@@ -129,3 +98,4 @@ int main(){
     }
     return 0;
 }
+
